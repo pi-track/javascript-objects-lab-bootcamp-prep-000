7911c8f3277d46f392d92941fd932dcf1dbfd051
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-03 19:21:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   newObj = Object.assign({},object)
-  delete newObj.key
+  delete newObj[key]
   return newObj
 }
 

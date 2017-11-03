119e8a5395dce67f002c9fde8cb2148bcf2d4b1a
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-03 19:09:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return
+  return Object.assign(object,{key,value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
